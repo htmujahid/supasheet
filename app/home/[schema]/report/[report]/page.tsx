e395d528f20c23caaebf187f4c0137699a831179
htmujahid <htmujahid@gmail.com>
2025-10-22 07:48:02 +0500

    refactor: improve notFound condition in HomeResourcePage for better error handling

--- app/home/[schema]/report/[report]/page.tsx
+++ app/home/[schema]/report/[report]/page.tsx
@@ -30,7 +30,7 @@ async function HomeResourcePage(props: {
     loadReportData(schema, report, search),
   ]);
 
-  if (!columnsSchema || !data) {
+  if (!columnsSchema?.length || !data.total) {
     notFound();
   }
 
