311f66558a828b385d33825415358b88f6e88498
htmujahid <htmujahid@gmail.com>
2025-11-23 08:42:42 +0500

    feat: simplify NotFoundPage component structure and remove SidebarInset

--- app/home/not-found.tsx
+++ app/home/not-found.tsx
@@ -21,35 +21,31 @@ export const generateMetadata = async () => {
 
 async function NotFoundPage() {
   return (
-    <>
-      <SidebarInset>
-        <div className={"flex h-screen flex-1 flex-col"}>
-          <div
-            className={
-              "container m-auto flex w-full flex-1 flex-col items-center justify-center"
-            }
-          >
-            <Empty>
-              <EmptyHeader>
-                <EmptyMedia variant="icon">
-                  <FileQuestionIcon />
-                </EmptyMedia>
-                <EmptyTitle>404 - Page Not Found</EmptyTitle>
-                <EmptyDescription>
-                  The page you&apos;re looking for doesn&apos;t exist or has
-                  been moved.
-                </EmptyDescription>
-              </EmptyHeader>
-              <EmptyContent>
-                <Button asChild>
-                  <Link href="/">Back to Home</Link>
-                </Button>
-              </EmptyContent>
-            </Empty>
-          </div>
-        </div>
-      </SidebarInset>
-    </>
+    <div className={"flex h-screen flex-1 flex-col"}>
+      <div
+        className={
+          "container m-auto flex w-full flex-1 flex-col items-center justify-center"
+        }
+      >
+        <Empty>
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <FileQuestionIcon />
+            </EmptyMedia>
+            <EmptyTitle>404 - Page Not Found</EmptyTitle>
+            <EmptyDescription>
+              The page you&apos;re looking for doesn&apos;t exist or has
+              been moved.
+            </EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent>
+            <Button asChild>
+              <Link href="/">Back to Home</Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      </div>
+    </div>
   );
 }
 
--- app/home/user/not-found.tsx
+++ app/home/user/not-found.tsx
@@ -11,7 +11,6 @@ import {
   EmptyMedia,
   EmptyTitle,
 } from "@/components/ui/empty";
-import { SidebarInset } from "@/components/ui/sidebar";
 
 export const generateMetadata = async () => {
   return {
@@ -21,35 +20,31 @@ export const generateMetadata = async () => {
 
 async function NotFoundPage() {
   return (
-    <>
-      <SidebarInset>
-        <div className="flex min-h-[calc(100svh-183px)] items-center justify-center">
-          <div
-            className={
-              "container m-auto flex w-full flex-1 flex-col items-center justify-center"
-            }
-          >
-            <Empty>
-              <EmptyHeader>
-                <EmptyMedia variant="icon">
-                  <FileQuestionIcon />
-                </EmptyMedia>
-                <EmptyTitle>404 - Page Not Found</EmptyTitle>
-                <EmptyDescription>
-                  The page you&apos;re looking for doesn&apos;t exist or has
-                  been moved.
-                </EmptyDescription>
-              </EmptyHeader>
-              <EmptyContent>
-                <Button asChild>
-                  <Link href="/">Back to Home</Link>
-                </Button>
-              </EmptyContent>
-            </Empty>
-          </div>
-        </div>
-      </SidebarInset>
-    </>
+    <div className="flex min-h-[calc(100svh-183px)] items-center justify-center">
+      <div
+        className={
+          "container m-auto flex w-full flex-1 flex-col items-center justify-center"
+        }
+      >
+        <Empty>
+          <EmptyHeader>
+            <EmptyMedia variant="icon">
+              <FileQuestionIcon />
+            </EmptyMedia>
+            <EmptyTitle>404 - Page Not Found</EmptyTitle>
+            <EmptyDescription>
+              The page you&apos;re looking for doesn&apos;t exist or has
+              been moved.
+            </EmptyDescription>
+          </EmptyHeader>
+          <EmptyContent>
+            <Button asChild>
+              <Link href="/">Back to Home</Link>
+            </Button>
+          </EmptyContent>
+        </Empty>
+      </div>
+    </div>
   );
 }
 
