5a9d83bb7520d3dba648a97b3645c4486e96ad81
htmujahid <htmujahid@gmail.com>
2025-11-25 17:36:38 +0500

    feat: replace Grid2X2PlusIcon with inline SVG in LogoImage component and add logo.svg file

--- components/app-logo.tsx
+++ components/app-logo.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 
-import { Grid2X2PlusIcon } from "lucide-react";
-
 import { cn } from "@/lib/utils";
 
 function LogoImage({
@@ -13,7 +11,10 @@ function LogoImage({
 }) {
   return (
     <div className={cn("flex w-fit items-center gap-2", className)}>
-      <Grid2X2PlusIcon width={width} />
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M12 3v17a1 1 0 0 1-1 1H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v6a1 1 0 0 1-1 1H3" />
+        <path d="M21 17v2a2 2 0 0 1-2 2h-2" />
+      </svg>
       <span>Supasheet.</span>
     </div>
   );
