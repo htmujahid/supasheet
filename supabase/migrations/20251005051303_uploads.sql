4a0a5ba111a087320b58cee3ec702f16c2c1fcec
Talha Mujahid <htmujahid@gmail.com>
2026-04-29 12:32:22 +0500

    Refactor uploads bucket policies and insert statement

--- supabase/migrations/20251005051303_uploads.sql
+++ supabase/migrations/20251005051303_uploads.sql
@@ -1,47 +1,48 @@
-insert into storage.buckets (id, name, public) 
-values ('uploads', 'uploads', true)
-ON CONFLICT DO NOTHING;
+DELETE FROM storage.buckets WHERE id = 'uploads';
 
-create policy "Enable read access to authenticated users for upload bucket" ON storage.buckets
-as permissive for select
-to authenticated
-using (
-    name = 'uploads'
-);
+DROP POLICY IF EXISTS enable_read_authenticated_uploads_bucket ON storage.buckets;
+DROP POLICY IF EXISTS enable_read_authorized_uploads_objects ON storage.objects;
+DROP POLICY IF EXISTS enable_insert_authorized_uploads_objects ON storage.objects;
+DROP POLICY IF EXISTS enable_update_authorized_uploads_objects ON storage.objects;
+DROP POLICY IF EXISTS enable_delete_authorized_uploads_objects ON storage.objects;
 
-create policy "Enable read access to authorized users for upload bucket" ON storage.objects
-as permissive for select
-to authenticated
-using (
-    bucket_id = 'uploads' and supasheet.has_permission(
+-- Bucket
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('uploads', 'uploads', true);
+
+-- Uploads bucket policies
+CREATE POLICY enable_read_authenticated_uploads_bucket ON storage.buckets
+AS PERMISSIVE FOR SELECT TO authenticated
+USING (name = 'uploads');
+
+CREATE POLICY enable_read_authorized_uploads_objects ON storage.objects
+AS PERMISSIVE FOR SELECT TO authenticated
+USING (
+    bucket_id = 'uploads' AND supasheet.has_permission(
         format('%s.%s:select', path_tokens[1], path_tokens[2])::supasheet.app_permission
     )
 );
 
-create policy "Enable insert access to authorized users for upload bucket" ON storage.objects
-as permissive for insert
-to authenticated
-with check (
-    bucket_id = 'uploads' and supasheet.has_permission(
+CREATE POLICY enable_insert_authorized_uploads_objects ON storage.objects
+AS PERMISSIVE FOR INSERT TO authenticated
+WITH CHECK (
+    bucket_id = 'uploads' AND supasheet.has_permission(
         format('%s.%s:insert', path_tokens[1], path_tokens[2])::supasheet.app_permission
     )
 );
 
-create policy "Enable update access to authorized users for upload bucket" ON storage.objects
-as permissive for update
-to authenticated
-using (
-    bucket_id = 'uploads' and supasheet.has_permission(
+CREATE POLICY enable_update_authorized_uploads_objects ON storage.objects
+AS PERMISSIVE FOR UPDATE TO authenticated
+USING (
+    bucket_id = 'uploads' AND supasheet.has_permission(
         format('%s.%s:update', path_tokens[1], path_tokens[2])::supasheet.app_permission
     )
 );
 
-create policy "Enable delete access to authorized users for upload bucket" ON storage.objects
-as permissive for delete
-to authenticated
-using (
-    bucket_id = 'uploads' and supasheet.has_permission(
+CREATE POLICY enable_delete_authorized_uploads_objects ON storage.objects
+AS PERMISSIVE FOR DELETE TO authenticated
+USING (
+    bucket_id = 'uploads' AND supasheet.has_permission(
         format('%s.%s:delete', path_tokens[1], path_tokens[2])::supasheet.app_permission
     )
 );
-
