f0a2b3bfb9073440727eeda8e81f168f5b14bc17
Talha Mujahid <htmujahid@gmail.com>
2026-04-29 12:31:22 +0500

    Update 20251005041214_general_storage.sql

--- supabase/migrations/20251005041214_general_storage.sql
+++ supabase/migrations/20251005041214_general_storage.sql
@@ -1,80 +1,64 @@
-insert into storage.buckets (id, name, public)
-values ('public', 'public', true), ('personal', 'personal', true)
+-- Cleanup
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('public', 'public', true), ('personal', 'personal', TRUE)
 ON CONFLICT DO NOTHING;
 
-create policy "Enable read access to authenticated users for public bucket" ON storage.buckets
-as permissive for select
-to authenticated
-using (
-    name = 'public'
-);
+DROP POLICY IF EXISTS "enable_read_authenticated_public_bucket" ON storage.buckets;
+DROP POLICY IF EXISTS "enable_read_all_public_objects" ON storage.objects;
+DROP POLICY IF EXISTS "enable_insert_authenticated_public_objects" ON storage.objects;
+DROP POLICY IF EXISTS "enable_update_owner_public_objects" ON storage.objects;
+DROP POLICY IF EXISTS "enable_delete_owner_public_objects" ON storage.objects;
+DROP POLICY IF EXISTS "enable_read_authenticated_personal_bucket" ON storage.buckets;
+DROP POLICY IF EXISTS "enable_read_owner_personal_objects" ON storage.objects;
+DROP POLICY IF EXISTS "enable_insert_owner_personal_objects" ON storage.objects;
+DROP POLICY IF EXISTS "enable_update_owner_personal_objects" ON storage.objects;
+DROP POLICY IF EXISTS "enable_delete_owner_personal_objects" ON storage.objects;
 
-create policy "Enable read access to all users for public bucket" ON storage.objects
-as permissive for select
-to public
-using (
-    bucket_id = 'public'
-);
+-- Buckets
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('public', 'public', true), ('personal', 'personal', true)
+ON CONFLICT DO NOTHING;
 
-create policy "Enable insert access to authenticated users for public bucket" ON storage.objects
-as permissive for insert
-to authenticated
-with check (
-    bucket_id = 'public'
-);
+-- Public bucket policies
+CREATE POLICY enable_read_authenticated_public_bucket ON storage.buckets
+AS PERMISSIVE FOR SELECT TO authenticated
+USING (name = 'public');
 
-create policy "Enable update access to owners for public bucket" ON storage.objects
-as permissive for update
-to authenticated
-using (
-    bucket_id = 'public' and owner_id::uuid = (select auth.uid()::uuid)
-)
-with check (
-    bucket_id = 'public' and owner_id::uuid = (select auth.uid()::uuid)
-);
+CREATE POLICY enable_read_all_public_objects ON storage.objects
+AS PERMISSIVE FOR SELECT TO public
+USING (bucket_id = 'public');
 
-create policy "Enable delete access to owners for public bucket" ON storage.objects
-as permissive for delete
-to authenticated
-using (
-    bucket_id = 'public' and owner_id::uuid = (select auth.uid()::uuid)
-);
+CREATE POLICY enable_insert_authenticated_public_objects ON storage.objects
+AS PERMISSIVE FOR INSERT TO authenticated
+WITH CHECK (bucket_id = 'public');
 
+CREATE POLICY enable_update_owner_public_objects ON storage.objects
+AS PERMISSIVE FOR UPDATE TO authenticated
+USING (bucket_id = 'public' AND owner_id::uuid = (SELECT auth.uid()::uuid))
+WITH CHECK (bucket_id = 'public' AND owner_id::uuid = (SELECT auth.uid()::uuid));
 
-create policy "Enable read access to authenticated users for personal bucket" ON storage.buckets
-as permissive for select
-to authenticated
-using (
-    name = 'personal'
-);
+CREATE POLICY enable_delete_owner_public_objects ON storage.objects
+AS PERMISSIVE FOR DELETE TO authenticated
+USING (bucket_id = 'public' AND owner_id::uuid = (SELECT auth.uid()::uuid));
 
-create policy "Enable read access to owners for personal bucket" ON storage.objects
-as permissive for select
-to authenticated
-using (
-    bucket_id = 'personal' and owner_id::uuid = (select auth.uid()::uuid)
-);
+-- Personal bucket policies
+CREATE POLICY enable_read_authenticated_personal_bucket ON storage.buckets
+AS PERMISSIVE FOR SELECT TO authenticated
+USING (name = 'personal');
 
-create policy "Enable insert access to owners for personal bucket" ON storage.objects
-as permissive for insert
-to authenticated
-with check (
-    bucket_id = 'personal' and owner_id::uuid = (select auth.uid()::uuid)
-);
+CREATE POLICY enable_read_owner_personal_objects ON storage.objects
+AS PERMISSIVE FOR SELECT TO authenticated
+USING (bucket_id = 'personal' AND owner_id::uuid = (SELECT auth.uid()::uuid));
 
-create policy "Enable update access to owners for personal bucket" ON storage.objects
-as permissive for update
-to authenticated
-using (
-    bucket_id = 'personal' and owner_id::uuid = (select auth.uid())
-)
-with check (
-    bucket_id = 'personal' and owner_id::uuid = (select auth.uid())
-);
+CREATE POLICY enable_insert_owner_personal_objects ON storage.objects
+AS PERMISSIVE FOR INSERT TO authenticated
+WITH CHECK (bucket_id = 'personal' AND owner_id::uuid = (SELECT auth.uid()::uuid));
 
-create policy "Enable delete access to owners for personal bucket" ON storage.objects
-as permissive for delete
-to authenticated
-using (
-    bucket_id = 'personal' and owner_id::uuid = (select auth.uid())
-);
+CREATE POLICY enable_update_owner_personal_objects ON storage.objects
+AS PERMISSIVE FOR UPDATE TO authenticated
+USING (bucket_id = 'personal' AND owner_id::uuid = (SELECT auth.uid()::uuid))
+WITH CHECK (bucket_id = 'personal' AND owner_id::uuid = (SELECT auth.uid()::uuid));
+
+CREATE POLICY enable_delete_owner_personal_objects ON storage.objects
+AS PERMISSIVE FOR DELETE TO authenticated
+USING (bucket_id = 'personal' AND owner_id::uuid = (SELECT auth.uid()::uuid));
